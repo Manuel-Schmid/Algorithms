28bed2b8b66412bc97fcf73c9d09b08c9b740c15
Manuel Schmid <manysch3@gmail.com>
2021-09-02 23:00:46 +0200

    added Pascal-Triangle algorithm

--- random_algs.js
+++ random_algs.js
@@ -15,17 +15,36 @@ function isPalindrome(word) {
     return f === s
 }
 
-CollatzConjecture(100, 100)
+// CollatzConjecture(41, 72)
 
-function CollatzConjecture(n, counter) {
-    if (counter <= 0) return
-    else counter--
+function CollatzConjecture(n, repeats) {
+    if (repeats <= 0) return
+    else repeats--
     let num
     isEven(n) ? num = n / 2 : num = n * 3 + 1
     console.log(num)
-    CollatzConjecture(num, counter)
+    CollatzConjecture(num, repeats)
 }
 
 function isEven(num) {
     return num % 2 === 0
+}
+
+// pascalTriangle(6)
+
+function pascalTriangle(rows) {
+    let pTriangle = new Array(rows)
+    for (let i = 0; i < rows; i++) {
+        let row = new Array(i + 1)
+        row[0] = 1;
+        row[row.length - 1] = 1;
+        for (let j = 1; j < row.length - 1; j++) {
+            let prevRow = pTriangle[i-1]
+            row[j] = prevRow[j] + prevRow[j - 1];
+        }
+        pTriangle[i] = row;
+    }
+    for (let row of pTriangle) {
+        console.log(row)
+    }
 }
