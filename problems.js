701c6f18e741e4a34af36282273f7f060f3ebe90
Manuel Schmid <manysch3@gmail.com>
2021-09-05 22:50:23 +0200

    added like-algorithm

--- problems.js
+++ problems.js
@@ -50,7 +50,7 @@ function nb_year(p0, percent, aug, p) {
 * String will never be empty and you do not need to account for different data types.
 * */
 
-console.log(shortestWord('why does thine kin ceaselessly assume the worst possible outcome'))
+// console.log(shortestWord('why does thine kin ceaselessly assume the worst possible outcome'))
 
 function shortestWord(wordsString) {
     const words = wordsString.split(' ')
@@ -59,4 +59,39 @@ function shortestWord(wordsString) {
         if (word.length < shortestLength) shortestLength = word.length
     }
     return shortestLength
+}
+
+/* ***************************************************************************************** */
+
+
+/*
+* You probably know the "like" system from Facebook and other pages.
+* People can "like" blog posts, pictures or other items. We want to create the
+* text that should be displayed next to such an item.
+
+Implement the function likes which takes an array containing the names of people
+* that like an item. It must return the display text as shown in the examples:
+*
+*
+likes [] -- must be "no one likes this"
+likes ["Peter"] -- must be "Peter likes this"
+likes ["Jacob", "Alex"] -- must be "Jacob and Alex like this"
+likes ["Max", "John", "Mark"] -- must be "Max, John and Mark like this"
+likes ["Alex", "Jacob", "Mark", "Max"] -- must be "Alex, Jacob and 2 others like this"
+*
+* For 4 or more names, the number in and 2 others simply increases.
+* */
+
+console.log(likes([]))
+console.log(likes(["Peter"]))
+console.log(likes(["Jacob", "Alex"]))
+console.log(likes(["Max", "John", "Mark"]))
+console.log(likes(["Alex", "Jacob", "Mark", "Max"]))
+
+function likes(people) {
+    if (people.length <= 0) return "no one likes this"
+    else if (people.length === 1) return people[0] + " likes this"
+    else if (people.length === 2) return people[0] + " and " + people[1] + " like this"
+    else if (people.length === 3) return people[0] + ", " + people[1] + " and " + people[2] + " like this"
+    else if (people.length >= 4) return people[0] + ", " + people[1] + " and " + (people.length-2) + " others like this"
 }
