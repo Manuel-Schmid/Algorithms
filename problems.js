10d84c875d7edd87d1fb12703e6af0a1a1c05eb7
Manuel Schmid <manysch3@gmail.com>
2021-09-06 18:46:31 +0200

    added function to concatenate squared digits of a number

--- problems.js
+++ problems.js
@@ -138,3 +138,23 @@ function calcNarcissisticNumbersInRange(range) {
     return narcissisticNumbers
 }
 
+
+/* ***************************************************************************************** */
+
+/*
+* Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+* For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+* */
+
+console.log(concatenateSquaredDigits(9119))
+
+function concatenateSquaredDigits(number) {
+    let result = '';
+    for (let i = 0; i < number.toString().length; i++) {
+        result += Math.pow(parseInt(number.toString()[i]), 2)
+    }
+    return parseInt(result);
+}
+
+/* ***************************************************************************************** */
+
