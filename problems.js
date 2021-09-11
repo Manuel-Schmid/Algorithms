321bc52085a98a7c27bc90580f9855989a5f09ea
Manuel Schmid <manysch3@gmail.com>
2021-09-11 21:32:50 +0200

    finished implementing intDiff function

--- problems.js
+++ problems.js
@@ -215,11 +215,15 @@ function duplicateCount(text) {
 Determine the number of times where two integers in the array have a difference of n.
 * */
 
-intDiff([1, 1, 5, 6, 9, 16, 27], 4) // 3
-intDiff([1, 1, 3, 3], 2) // 4
-const intDiff = (arr, n) => {
-    for (let i of arr) {
-
+console.log(intDiff([1, 1, 5, 6, 9, 16, 27], 4)) // 3
+console.log(intDiff([1, 1, 3, 3], 2)) // 4
+function intDiff(arr, n) {
+    let intDiffsCount = 0
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] - arr[i] === n || arr[i] - arr[j] === n) intDiffsCount++
+        }
     }
+    return intDiffsCount
 }
 
