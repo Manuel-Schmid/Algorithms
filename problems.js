77624045b9c2a5e66e3437458e8a4b985325d9a3
Manuel Schmid <manysch3@gmail.com>
2021-09-12 02:21:51 +0200

    added algorithm to calculate physics of a bouncing ball

--- problems.js
+++ problems.js
@@ -300,9 +300,9 @@ function order(words) {
 * remember: sequence can be a string or an array
 * */
 
-console.log(uniqueInOrder('AAAABBBCCDAABBB')) // == ['A', 'B', 'C', 'D', 'A', 'B']
-console.log(uniqueInOrder('ABBCcAD'))         // == ['A', 'B', 'C', 'c', 'A', 'D']
-console.log(uniqueInOrder([1,2,2,3,3]))       // == [1,2,3]
+// console.log(uniqueInOrder('AAAABBBCCDAABBB')) // == ['A', 'B', 'C', 'D', 'A', 'B']
+// console.log(uniqueInOrder('ABBCcAD'))         // == ['A', 'B', 'C', 'c', 'A', 'D']
+// console.log(uniqueInOrder([1,2,2,3,3]))       // == [1,2,3]
 
 function uniqueInOrder(sequence) {
     if (sequence.length === 0) return []
@@ -317,9 +317,38 @@ function uniqueInOrder(sequence) {
 
 
 /* *****************************************************************************************
-*
-*
+*A child is playing with a ball on the nth floor of a tall building. The height of this floor, h, is known.
+
+He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+
+His mother looks out of a window 1.5 meters from the ground.
+
+How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+
+Three conditions must be met for a valid experiment:
+- Float parameter "h" in meters must be greater than 0
+- Float parameter "bounce" must be greater than 0 and less than 1
+- Float parameter "window" must be less than h.
+If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+
+Note:
+The ball can only be seen if the height of the rebounding ball is strictly greater than the window parameter.
 * */
+
+// console.log(bouncingBall(3, 0.66, 1.5)) // 3
+// console.log(bouncingBall(3, 1, 1.5)) // -1
+// console.log(bouncingBall(30.0, 0.66, 1.5)) // 15
+
+function bouncingBall(h,  bounce,  window) {
+    if (h <= 0 || bounce >= 1 || bounce <= 0 || window >= h) return -1
+    let rebounds = -1
+    while (h > window) {
+        rebounds += 2
+        h *= bounce
+    }
+    return rebounds
+}
+
 
 
 /* *****************************************************************************************
