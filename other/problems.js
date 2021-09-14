b3fc58fa672e434f92939f298a51149503acdc9a
Manuel Schmid <manysch3@gmail.com>
2021-09-14 23:37:36 +0200

    added function to find out which of the given numbers differs from the others

--- other/problems.js
+++ other/problems.js
@@ -423,10 +423,28 @@ function countChars(str) {
 
 
 /* *****************************************************************************************
-*
-*
+Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given
+numbers differs from the others. Bob observed that one number usually differs from the others in evenness.
+Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different
+in evenness, and return a position of this number.
+
+Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements
+start from 1 (not 0)
 * */
 
+console.log(iqTest("2 4 7 8 10")) // => 3 // Third number is odd, while the rest of the numbers are even
+console.log(iqTest("1 2 1 1")) // => 2 // Second number is even, while the rest of the numbers are odd
+console.log(iqTest("1 2 5 4")) // => 0 // There was no irregular number.
+
+function iqTest(numbers) {
+    let allEvenNumbers = numbers.split(' ').filter(num => num % 2 === 0)
+    if (allEvenNumbers.length === 1) { // if only one even number
+        return numbers.split(' ').join(',') + " => " + (numbers.split(' ').indexOf(allEvenNumbers.toString()) + 1) + '. number is even, while the rest of the numbers are odd'
+    } else if (allEvenNumbers.length === numbers.split(' ').length-1) { // if only one odd number
+        let oddNumber = numbers.split(' ').filter(num => num % 2 !== 0)
+        return numbers.split(' ').join(',') + " => " + (numbers.split(' ').indexOf(oddNumber.toString()) + 1) + '. number is odd, while the rest of the numbers are even'
+    } else return 'There was no irregular number.'
+}
 
 /* *****************************************************************************************
 *
