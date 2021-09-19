481c1fc3e8525d1911c38028e4c7acd79c7ffeb2
Manuel Schmid <manysch3@gmail.com>
2021-09-19 16:01:20 +0200

    implemented algorithm to convert roman numbers to integers

--- other/problems.js
+++ other/problems.js
@@ -671,9 +671,54 @@ X can be placed before L (50) and C (100) to make 40 and 90.
 C can be placed before D (500) and M (1000) to make 400 and 900.
 Given a roman numeral, convert it to an integer.
 * */
+console.log(romanToInt("MMXXI")) // -> 2021
+console.log(romanToInt("IX")) // -> 9
+console.log(romanToInt("XIV")) // -> 14
 
-function romanToInt() {
-    
+function romanToInt(s) {
+    let result = 0;
+    for (let i in s) {
+        if (s[i] == "I") {
+            result += 1;
+        } else if (s[i] == "V") {
+            if (i > 0 && s[i - 1] == "I"){
+                result += 3
+            } else {
+                result += 5
+            }
+        } else if (s[i] == "X") {
+            if (i > 0 && s[i - 1] == "I"){
+                result += 8
+            } else {
+                result += 10
+            }
+        } else if (s[i] == "L") {
+            if (i > 0 && s[i - 1] == "X"){
+                result += 30
+            } else {
+                result += 50
+            }
+        } else if (s[i] == "C") {
+            if (i > 0 && s[i - 1] == "X"){
+                result += 80
+            } else {
+                result += 100
+            }
+        } else if (s[i] == "D") {
+            if (i > 0 && s[i - 1] == "C"){
+                result += 300
+            } else {
+                result += 500
+            }
+        } else if (s[i] == "M") {
+            if (i > 0 && s[i - 1] == "C"){
+                result += 800
+            } else {
+                result += 1000
+            }
+        }
+    }
+    return result
 }
 
 /* *****************************************************************************************
