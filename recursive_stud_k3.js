e688b86ec4794f8eaade6cc4dc1d8d7c86e61544
Manuel Schmid <manysch3@gmail.com>
2021-09-29 08:38:33 +0200

    implemented recursive sorting algorithm (merge)

--- recursive_stud_k3.js
+++ recursive_stud_k3.js
@@ -12,3 +12,27 @@ function insertionSort(array) {
     }
     return array
 }
+
+console.log(recursiveSort([2,4,5,4,3,6,4,33,7,9,876,54,7,654,3]))
+
+function recursiveSort(arr) {
+    if (arr.length <= 1) return arr
+    const half = arr.length / 2;
+    const left = arr.splice(0, half);
+    return merge(recursiveSort(left), recursiveSort(arr)); // arr = right
+}
+
+function merge(left, right) {
+    let sortedArr = [];
+
+    while (left.length && right.length) {
+        // insert the smallest element to the sortedArr
+        if (left[0] < right[0]) {
+            sortedArr.push(left.shift());
+        } else {
+            sortedArr.push(right.shift());
+        }
+    }
+
+    return [...sortedArr, ...left, ...right];
+}
